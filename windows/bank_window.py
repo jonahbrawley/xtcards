9e1f8ab514cc89694d5490e7e79699e24c9734ac
Jonah Brawley <jonahbrawley@gmail.com>
2024-02-26 13:09:49 -0800

    Bank layout improvements

--- windows/bank_window.py
+++ windows/bank_window.py
@@ -8,32 +8,40 @@ class bankWindow(pygame_gui.elements.UIWindow):
                         window_display_title='The Holy Bank',
                         object_id='#setup_window',
                         draggable=False)
-        self.bank_label = pygame_gui.elements.UILabel(pygame.Rect((0, 20), (180, 40)),
-                                                                    "Value:",
-                                                                    manager=manager,
-                                                                    object_id="header_game",
-                                                                    container=self,
-                                                                    parent_element=self,
-                                                                    anchors={
-                                                                        "top": "top",
-                                                                        "centerx": "centerx"
-                                                                    })
-        self.value_label = pygame_gui.elements.UILabel(pygame.Rect((0, 20), (180, 40)),
-                                                                    "0",
-                                                                    manager=manager,
-                                                                    object_id="header_game",
-                                                                    container=self,
-                                                                    parent_element=self,
-                                                                    anchors={
-                                                                        "top_target": self.bank_label,
-                                                                        "centerx": "centerx"
-                                                                    })
-        self.log_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((0, -60), ((100), 40)),
-                                                            text='Log',
-                                                            manager=manager,
-                                                            container=self,
-                                                            parent_element=self,
-                                                            anchors={
-                                                                "bottom": "bottom",
-                                                                "centerx": "centerx"
-                                                            })
+
+        windowSpacer = pos.height * .1
+        logButtonWidth = pos.width * .2
+
+        valueRect = pygame.Rect((0, windowSpacer), (pos.width, windowSpacer))
+        amountRect = pygame.Rect((0, windowSpacer), (pos.width, 40))
+        logRect = pygame.Rect((0, -(windowSpacer*2)), (logButtonWidth, 40))
+
+        self.bank_label = pygame_gui.elements.UILabel(valueRect,
+                                                    "Jackpot:",
+                                                    manager=manager,
+                                                    object_id="value_header",
+                                                    container=self,
+                                                    parent_element=self,
+                                                    anchors={
+                                                        "top": "top",
+                                                        "centerx": "centerx"
+                                                    })
+        self.value_label = pygame_gui.elements.UILabel(amountRect,
+                                                    "0",
+                                                    manager=manager,
+                                                    object_id="header_game",
+                                                    container=self,
+                                                    parent_element=self,
+                                                    anchors={
+                                                        "top_target": self.bank_label,
+                                                        "centerx": "centerx"
+                                                    })
+        self.log_button = pygame_gui.elements.UIButton(logRect,
+                                                    text='Log',
+                                                    manager=manager,
+                                                    container=self,
+                                                    parent_element=self,
+                                                    anchors={
+                                                        "bottom": "bottom",
+                                                        "centerx": "centerx"
+                                                    })
